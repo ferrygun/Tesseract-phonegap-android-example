22e45517503d2f0552e59b37660631021f620e3c
ferrygun <ferrygun@yahoo.com>
2015-05-14 15:32:25 +0800

    Update tesseractPlugin.js

--- plugins/com.tesseract.phonegap.tesseractPlugin/www/tesseractPlugin.js
+++ plugins/com.tesseract.phonegap.tesseractPlugin/www/tesseractPlugin.js
@@ -1,13 +1,15 @@
-var tesseractPlugin = {
-    createEvent: function(str, Callback) {
-        cordova.exec(callback, function(err) {
-		callback('Nothing to echo.');
-			},
-			"TesseractPlugin",			//Service: Mapped to native java.class
-			"addTesseractPluginEntry",	//Action: The action name to call into on the native side
-			[{							//An array of arguments 
-			str							//This string should contain path to image
-			}]);						          
-     }
-}
-module.exports = tesseract;
+
+	var tesseractPlugin = {
+		createEvent: function(str, callback) {
+			cordova.exec(callback, function(err) {
+			callback('Nothing to echo.');
+				},
+				"TesseractPlugin",			//Service: Mapped to native java.class
+				"addTesseractPluginEntry",	//Action: The action name to call into on the native side
+				[str]							//An array of arguments 
+				);						//This string should contain path to image
+												  
+		 }
+	}
+	module.exports = tesseractPlugin;
+
